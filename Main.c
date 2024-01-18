088d73558f9f4bc7b0e083e06ba71314af7b889b
Connor Waiter <connor.waiter.2022@uni.strath.ac.uk>
2024-01-18 16:28:30 +0000

    Shell now exits when 'exit' or CRTL+D is detected

--- Main.c
+++ Main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* UserPrompt();
 
 int main(){
-    char input[511]="";
-    printf("|-o-| ");
-    fgets(input, sizeof(input), stdin);
-    printf("\nCommand entered: %s",input); 
+    while(1){
+        char* input = UserPrompt();
+
+        if (!(strcmp(input,"\n")==0))
+        {
+            printf("Command entered: %s",input);
+        }
+    }
+}
+
+//Returns the users' input, or quits if 'exit' is inputted or CTRL+D is detected
+char* UserPrompt(){
+    char* input = (char*)malloc(511);
+    printf("\n|-o-| ");
+    char* fgetsResult = fgets(input, sizeof(input), stdin);
+
+    if((fgetsResult==NULL)|(strcmp(input,"exit\n")==0)){
+        printf("\n");
+        exit(1);
+    }
+
+    return input;
 }

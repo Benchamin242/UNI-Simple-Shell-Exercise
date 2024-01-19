ce3a49979e55b6dc0a5b2346daf75d67b45babcc
LukeAG777 <luke.smythe.2021@uni.strath.ac.uk>
2024-01-19 14:18:20 +0000

    User input is now tokenized and stored in an array as a list of commands

--- Main.c
+++ Main.c
@@ -3,23 +3,32 @@
 #include <string.h>
 
 char* UserPrompt();
+char* delimiters=" \t<>|;&";
 
 int main(){
     while(1){
-        char* input = UserPrompt();
-
+        char* input=UserPrompt();
         if (!(strcmp(input,"\n")==0))
         {
-            printf("Command entered: %s",input);
+            //tokenizes string and stores it
+            char commands[50][511];
+            char* token=strtok(input, delimiters);
+            int i=0;
+            while(token!=NULL){
+                printf("Command entered: %s\n",token);
+                strcpy(commands[i], token);
+                i++;
+                token=strtok(NULL, delimiters);
+            }
         }
     }
 }
 
 //Returns the users' input, or quits if 'exit' is inputted or CTRL+D is detected
 char* UserPrompt(){
-    char* input = (char*)malloc(511);
+    char* input=(char *)malloc(512 * sizeof(char));
     printf("\n|-o-| ");
-    char* fgetsResult = fgets(input, sizeof(input), stdin);
+    char* fgetsResult = fgets(input, 511, stdin);
 
     if((fgetsResult==NULL)|(strcmp(input,"exit\n")==0)){
         printf("\n");
